b0bb72c6f05f6839f9db93bd1a633403ca55d5af
Meer Uzair <meer.uxair007@gmail.com>
2025-01-03 22:13:27 +0530

    wip

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,17 +1,36 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const app = express();
+app.use(express.json());
 
-// password logic here
-
-app.post("/hooks/catch/:userId/:connectId", (req: Request, res: Response) => {
+// https://hooks.zapier.com/hooks/catch/17043103/22b8496/
+// password logic
+app.post("/hooks/catch/:userId/:connectId", async (req, res) => {
   const userId = req.params.userId;
   const connectId = req.params.connectId;
+  const body = req.body;
 
-// store  in db a new trigger 
+  // store in db a new trigger
+  await client.$transaction(async (tx) => {
+    const run = await tx.connectRun.create({
+      data: {
+        connectId,
+        metadata: body,
+      },
+    });
 
-// push  it on to a queue (kafka/redis)
-
-
-
+    await tx.connectRunOutbox.create({
+      data: {
+        connectRunId: run.id,
+      },
+    });
+  });
+  res.json({
+    message: "Webhook received",
+  });
 });
+
+app.listen(3002);

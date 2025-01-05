3bacbd7a3a6e3d8a2c80e3088fb653b446188a56
Meer Uzair <meer.uxair007@gmail.com>
2025-01-05 18:50:57 +0530

    wip

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,6 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+import { Kafka } from "kafkajs";
 
+const prisma = new PrismaClient();
 
+export const TOPIC_NAME = "outbox-topic";
 
-while(1){
-    
+const kafka = new Kafka({
+  clientId: "outbox-processor",
+  brokers: ["localhost:9092"],
+});
+
+async function main() {
+  const producer = kafka.producer();
+
+  await producer.connect();
+
+  while (1) {
+    const pendingRows = await prisma.connectRunOutbox.findMany({
+      where: {},
+      take: 10,
+    });
+
+    await producer.send({
+      topic: TOPIC_NAME,
+      messages: pendingRows.map((row) => ({
+        key: row.connectRunId,
+        value: JSON.stringify(row.connectRunId),
+      })),
+    });
+  }
 }
+
+main();

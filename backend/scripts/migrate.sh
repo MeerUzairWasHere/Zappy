4cf57c08fa05c4780fed48dcc40e4f884d992307
Meer Uzair <meer.uxair007@gmail.com>
2025-01-08 23:35:37 +0530

    removed script files

--- backend/scripts/migrate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Get the migration name from the first argument, default to "init" if not provided
-MIGRATION_NAME=${1:-init}
-npx prisma migrate dev --name "$MIGRATION_NAME"
-npm run start

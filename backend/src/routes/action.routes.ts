2d6b4a427a717040b1506b8fcd31346260410c30
Meer Uzair <meer.uxair007@gmail.com>
2025-01-09 00:32:44 +0530

    added new route

---
+++ backend/src/routes/action.routes.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Router, Request, Response } from "express";
+import { authenticateUser } from "../middlewares/authentication";
+
+const router = Router();
+
+import { getAvailableAction } from "../controllers/action.controller";
+
+router.route("/available").post(
+  (req: Request, res: Response, next: NextFunction) =>
+    authenticateUser(req, res, next),
+  (req: Request, res: Response, next: NextFunction) =>
+    getAvailableAction(req, res)
+);
+
+export default router;

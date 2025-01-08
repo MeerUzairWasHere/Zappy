e2388bda460fc4662e68811c99b7df342b99860a
Meer Uzair <meer.uxair007@gmail.com>
2025-01-09 00:30:09 +0530

    added new route

---
+++ backend/src/routes/trigger.routes.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Router, Request, Response } from "express";
+import { authenticateUser } from "../middlewares/authentication";
+
+const router = Router();
+
+import { getAvailableTrigger } from "../controllers/trigger.controller";
+
+router.route("/available").post(
+  (req: Request, res: Response, next: NextFunction) =>
+    authenticateUser(req, res, next),
+  (req: Request, res: Response, next: NextFunction) => getAvailableTrigger(req, res),
+);
+
+
+export default router;

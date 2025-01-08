20d233e79d318462ac0cc143302fe14c8e31b5f2
Meer Uzair <meer.uxair007@gmail.com>
2025-01-08 14:13:49 +0530

    added worker

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -4,3 +4,32 @@ import { Kafka } from "kafkajs";
 const prisma = new PrismaClient();
 
 export const TOPIC_NAME = "zap-events";
+
+const kafka = new Kafka({
+  clientId: "outbox-processor",
+  brokers: ["localhost:9092"],
+});
+
+async function main() {
+  const consumer = kafka.consumer({ groupId: "main-worker" });
+
+  await consumer.connect();
+
+  await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const offset = JSON.parse(message.offset.toString());
+      const value = JSON.parse(message.value.toString());
+
+      console.log("Topic: ", topic);
+      console.log("Partion: ", partition);
+      console.log(`Offset: ${offset}`);
+      console.log("Value: ", value);
+
+    //   await new Promise((r) => setTimeout(r, 3000));
+    },
+  });
+}
+
+main();

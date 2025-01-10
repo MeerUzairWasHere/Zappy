03f6e24a8a5fd5b3ff251848e9b13969463c4e15
Meer Uzair <meer.uxair007@gmail.com>
2025-01-10 21:51:21 +0530

    added create and delete methods

--- backend/src/controllers/trigger.controller.ts
+++ backend/src/controllers/trigger.controller.ts
@@ -2,9 +2,50 @@ import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
 
 import { prismaClient } from "../db";
+import { CreateAvailableTriggerInput } from "../types";
+
+export const createAvailableTrigger = async (
+  req: Request<{}, {}, CreateAvailableTriggerInput>,
+  res: Response
+) => {
+  const { name, image } = req.body;
+
+  const trigger = await prismaClient.availableTrigger.create({
+    data: {
+      name,
+      image,
+    },
+  });
+
+  res.status(StatusCodes.OK).json({ trigger });
+};
 
 export const getAvailableTrigger = async (req: Request, res: Response) => {
   const availableTriggers = await prismaClient.availableTrigger.findMany({});
 
   res.status(StatusCodes.OK).json({ availableTriggers });
+};
+
+export const deleteAvailableTrigger = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const exists = await prismaClient.availableTrigger.findFirst({
+    where: { id },
+  });
+
+  if (!exists) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `${id} does not exists!` });
+  }
+
+  await prismaClient.availableTrigger.delete({
+    where: {
+      id,
+    },
+  });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: `Available Trigger with id: ${id} is deleted successfully!` });
 };

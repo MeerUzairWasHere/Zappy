12ca4297cbdba282bb119987b5056eec0cdad241
Meer Uzair <meer.uxair007@gmail.com>
2025-01-10 20:30:44 +0530

    wip

--- backend/src/controllers/action.controller.ts
+++ backend/src/controllers/action.controller.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express";
 import { prismaClient } from "../db";
 
 export const getAvailableAction = async (req: Request, res: Response) => {
+  
   const availableActions = await prismaClient.availableAction.findMany({});
 
   res.status(StatusCodes.OK).json({ availableActions });

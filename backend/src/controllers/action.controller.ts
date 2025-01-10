11f1db608eb9647994e71b53f91223a97b713fc4
Meer Uzair <meer.uxair007@gmail.com>
2025-01-10 21:43:41 +0530

    added action methods

--- backend/src/controllers/action.controller.ts
+++ backend/src/controllers/action.controller.ts
@@ -2,9 +2,50 @@ import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
 
 import { prismaClient } from "../db";
+import { CreateAvailableActionInput } from "../types";
+
+export const createAvailableAction = async (
+  req: Request<{}, {}, CreateAvailableActionInput>,
+  res: Response
+) => {
+  const { name, image } = req.body;
+
+  const action = await prismaClient.availableAction.create({
+    data: {
+      name,
+      image,
+    },
+  });
+
+  res.status(StatusCodes.OK).json({ action });
+};
 
 export const getAvailableAction = async (req: Request, res: Response) => {
   const availableActions = await prismaClient.availableAction.findMany({});
 
   res.status(StatusCodes.OK).json({ availableActions });
+};
+
+export const deleteAvailableAction = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const exists = await prismaClient.availableAction.findFirst({
+    where: { id },
+  });
+
+  if (!exists) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `${id} does not exists!` });
+  }
+
+  await prismaClient.availableAction.delete({
+    where: {
+      id,
+    },
+  });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: `Available Action with id: ${id} is deleted successfully!` });
 };

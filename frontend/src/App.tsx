897c5882bdd475f626aa63bad379cb946db8b20a
Meer Uzair <meer.uxair007@gmail.com>
2025-01-16 22:31:43 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import GlobalLayout from "./layouts/GlobalLayout";
 
-const router = createBrowserRouter([{}]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <GlobalLayout />,
+    children: [
+      {
+        index: true,
+        element: <div>Home</div>,
+      },
+      {
+        path: "/about",
+        element: <div>About</div>,
+      },
+    ],
+  },
+]);
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,5 +38,3 @@ function App() {
 }
 
 export default App;
-
-

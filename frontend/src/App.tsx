ea34631c6a138864e0a9b8ede0ac5d77284cc211
Meer Uzair <meer.uxair007@gmail.com>
2025-01-16 22:17:04 +0530

    added RQ

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,26 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([{}]);
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
 
-// TODO: Add the following code to the App.tsx file
-{
-  /* <QueryClientProvider client={queryClient}>
-  <RouterProvider router={router} />
-  <ReactQueryDevtools initialIsOpen={false} />
-</QueryClientProvider>; */
-}
+

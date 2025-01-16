d068ea4bed9ed76054a69e8261b91f55305de37c
Meer Uzair <meer.uxair007@gmail.com>
2025-01-16 22:14:37 +0530

    added react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,17 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import customFetch from "./utils/fetch";
-import { Button } from "./components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([]);
 
 function App() {
-  function handleClick() {
-    async function login() {
-      try {
-        await customFetch.post("/auth/login", {
-          email: "meer.uxair007@gmail.com",
-          password: "144315_Khair.",
-        });
-        console.log("Login Successful");
-      } catch (error: any) {
-        if (error instanceof Error) {
-          console.log(error.message);
-        }
-      }
-    }
-    login();
-  }
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => handleClick()}>count is count</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button>Click me</Button>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
+// TODO: Add the following code to the App.tsx file
+{
+  /* <QueryClientProvider client={queryClient}>
+  <RouterProvider router={router} />
+  <ReactQueryDevtools initialIsOpen={false} />
+</QueryClientProvider>; */
+}

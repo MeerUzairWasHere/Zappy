c1ecd7121c5056225bd151e1d7dd7094bce30393
Meer Uzair <meer.uxair007@gmail.com>
2025-01-17 23:09:31 +0530

    Create index.ts

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as HeroSection } from "./LandingPage/HeroSection";
+export { default as FeaturesSection } from "./LandingPage/FeaturesSection";
+export { default as CTASection } from "./LandingPage/CTAsection";
+export { default as Footer } from "./LandingPage/Footer";

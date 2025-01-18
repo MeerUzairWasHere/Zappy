cd10a9074e8e97a1e33d9406d8a7e0f8d39c69a6
Meer Uzair <meer.uxair007@gmail.com>
2025-01-18 17:55:18 +0530

    removed devtools

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 //Third party imports
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // Pages imports
@@ -69,7 +68,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      {/* <ReactQueryDevtools position="left"  initialIsOpen={false} /> */}
+      <ReactQueryDevtools position="left"  initialIsOpen={false} />
     </QueryClientProvider>
   );
 }

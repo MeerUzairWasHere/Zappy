aac054b740ff34332972373e568fe532f175d624
Meer Uzair <meer.uxair007@gmail.com>
2025-01-20 00:11:47 +0530

    Update index.ts

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,3 +2,6 @@ export { default as LandingPage } from "./LandingPage";
 export { default as SignInPage } from "./SignInPage";
 export { default as SignUpPage } from "./SignUpPage";
 export { default as DashboardPage } from "./DashboardPage";
+export { default as ZapsPage } from "./ZapsPage";
+export { default as HistoryPage } from "./HistoryPage";
+export { default as ProfilePage } from "./ProfilePage";

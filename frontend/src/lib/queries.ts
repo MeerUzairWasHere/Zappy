faf3e2a7f03affffa15045e811306907909027f9
Meer Uzair <meer.uxair007@gmail.com>
2025-01-19 14:01:39 +0530

    Create queries.ts

---
+++ frontend/src/lib/queries.ts
@@ -0,0 +1,12 @@
+import customFetch from "@/utils/fetch";
+
+// Server state query
+export const userQuery = {
+  queryKey: ["user"],
+  queryFn: async () => {
+    const { data } = await customFetch.get("/users/current-user");
+    return data;
+  },
+};
+
+

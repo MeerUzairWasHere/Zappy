2e4362e28615fa73a94bb46b001c32ff646aee89
Meer Uzair <meer.uxair007@gmail.com>
2025-01-20 01:25:37 +0530

    Update SideBar.tsx

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,16 +1,11 @@
-import { userQuery } from "@/lib/queries";
-import { useQuery } from "@tanstack/react-query";
-import { History, Menu, Zap } from "lucide-react";
 import LogOutModal from "./LogOutModal";
 import clsx from "clsx";
 import { Link, NavLink } from "react-router-dom";
+import { Zap } from "lucide-react";
+import { links } from "@/lib/links";
+import SideBarUserProfile from "./SidebarUserProfile";
 
 const SideBar = () => {
-  // @ts-ignore
-  const {
-    data: { user },
-  } = useQuery(userQuery);
-
   return (
     <nav className="hidden lg:flex flex-col w-64 min-h-screen bg-white border-r border-neutral-200/20 sticky top-0">
       {/* <!-- Logo --> */}
@@ -23,7 +18,7 @@ const SideBar = () => {
       <LogOutModal />
 
       {/* <!-- User Profile --> */}
-      <SideBarUserProfile email={user?.email} name={user?.name} />
+      <SideBarUserProfile />
     </nav>
   );
 };
@@ -40,24 +35,6 @@ const SideBarLogo = () => {
     </Link>
   );
 };
-
-const links = [
-  {
-    to: "/dashboard",
-    label: "Dashboard",
-    icon: Menu,
-  },
-  {
-    to: "zaps",
-    label: "Zaps",
-    icon: Zap,
-  },
-  {
-    to: "history",
-    label: "History",
-    icon: History,
-  },
-];
 
 const SideBarLinks = () => {
   return (
@@ -82,30 +59,5 @@ const SideBarLinks = () => {
         </NavLink>
       ))}
     </div>
-  );
-};
-
-const SideBarUserProfile = ({
-  email,
-  name,
-}: {
-  email: string;
-  name: string;
-}) => {
-  return (
-    <Link to={"profile"} className="p-6 border-t border-neutral-200/20">
-      <div className="flex items-center">
-        <img
-          src="/avatar.png"
-          alt="User"
-          className="w-8 h-8 rounded-full transition-opacity duration-300 opacity-100"
-          loading="lazy"
-        />
-        <div className="ml-3">
-          <p className="text-sm font-medium text-gray-700">{name}</p>
-          <p className="text-xs text-gray-500">{email}</p>
-        </div>
-      </div>
-    </Link>
   );
 };

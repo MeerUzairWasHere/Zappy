e23cf5a3cf429f2263dfb180106dceb0a4046397
Meer Uzair <meer.uxair007@gmail.com>
2025-01-19 14:01:34 +0530

    removed user in client state

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -4,19 +4,11 @@ import { useEffect } from "react";
 
 import { Outlet, redirect, useNavigate, Navigate } from "react-router-dom";
 import { useDashboardStore } from "@/store/dashboardStore";
+import LogoutButton from "@/components/LogoutButton";
+import { userQuery } from "@/lib/queries";
 
-// Query configuration
-export const userQuery = {
-  queryKey: ["user"],
-  queryFn: async () => {
-    const { data } = await customFetch.get("/users/current-user");
-    // Use setState to update the store
-    useDashboardStore.setState({ user: data });
-    return data;
-  },
-};
 
-// Loader function
+
 export const loader = (queryClient: QueryClient) => async () => {
   try {
     return await queryClient.ensureQueryData(userQuery);
@@ -25,38 +17,37 @@ export const loader = (queryClient: QueryClient) => async () => {
   }
 };
 
-// Protected route wrapper
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const user = useDashboardStore((state) => state.user);
+  const { data: user } = useQuery(userQuery);
   return user ? children : <Navigate to="/sign-in" replace />;
 };
 
-// Dashboard Layout component
 const DashboardLayout = ({ queryClient }: { queryClient: QueryClient }) => {
   const navigate = useNavigate();
-  // const { data: userData } = useQuery(userQuery);
+  useQuery(userQuery);
   const { isAuthError, setIsAuthError, logoutUser } = useDashboardStore();
 
   // Set up axios interceptor
-  customFetch.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      if (error?.response?.status === 401) {
-        setIsAuthError(true);
+  useEffect(() => {
+    const interceptor = customFetch.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error?.response?.status === 401) {
+          setIsAuthError(true);
+        }
+        return Promise.reject(error);
       }
-      return Promise.reject(error);
-    }
-  );
+    );
+
+    return () => {
+      customFetch.interceptors.response.eject(interceptor);
+    };
+  }, [setIsAuthError]);
 
   // Handle auth errors
   useEffect(() => {
     if (!isAuthError) return;
-    const handleLogout = async () => {
-      await logoutUser();
-      queryClient.invalidateQueries();
-      navigate("/");
-    };
-    handleLogout();
+    logoutUser(queryClient, navigate);
   }, [isAuthError, logoutUser, queryClient, navigate]);
 
   return (
@@ -64,6 +55,11 @@ const DashboardLayout = ({ queryClient }: { queryClient: QueryClient }) => {
       <ProtectedRoute>
         <Outlet />
       </ProtectedRoute>
+      <LogoutButton
+        logoutUser={logoutUser}
+        queryClient={queryClient}
+        navigate={navigate}
+      />
     </main>
   );
 };

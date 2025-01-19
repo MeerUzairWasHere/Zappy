1a76670ccfaaad4d426fc7d51d3c1431cd9286be
Meer Uzair <meer.uxair007@gmail.com>
2025-01-20 01:25:43 +0530

    Create links.ts

---
+++ frontend/src/lib/links.ts
@@ -0,0 +1,19 @@
+import { History, Menu, Zap } from "lucide-react";
+
+export const links = [
+  {
+    to: "/dashboard",
+    label: "Dashboard",
+    icon: Menu,
+  },
+  {
+    to: "zaps",
+    label: "Zaps",
+    icon: Zap,
+  },
+  {
+    to: "history",
+    label: "History",
+    icon: History,
+  },
+];

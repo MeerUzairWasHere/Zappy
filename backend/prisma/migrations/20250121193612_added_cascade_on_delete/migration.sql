88d7357267b44a2908d0a6d0c02b64befa847ede
Meer Uzair <meer.uxair007@gmail.com>
2025-01-22 01:07:59 +0530

    added cascade onDelete

---
+++ backend/prisma/migrations/20250121193612_added_cascade_on_delete/migration.sql
@@ -0,0 +1,26 @@
+-- DropForeignKey
+ALTER TABLE "Action" DROP CONSTRAINT "Action_zapId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Trigger" DROP CONSTRAINT "Trigger_zapId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ZapRun" DROP CONSTRAINT "ZapRun_zapId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ZapRunOutbox" DROP CONSTRAINT "ZapRunOutbox_zapRunId_fkey";
+
+-- AlterTable
+ALTER TABLE "Zap" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true;
+
+-- AddForeignKey
+ALTER TABLE "Trigger" ADD CONSTRAINT "Trigger_zapId_fkey" FOREIGN KEY ("zapId") REFERENCES "Zap"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Action" ADD CONSTRAINT "Action_zapId_fkey" FOREIGN KEY ("zapId") REFERENCES "Zap"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ZapRun" ADD CONSTRAINT "ZapRun_zapId_fkey" FOREIGN KEY ("zapId") REFERENCES "Zap"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ZapRunOutbox" ADD CONSTRAINT "ZapRunOutbox_zapRunId_fkey" FOREIGN KEY ("zapRunId") REFERENCES "ZapRun"("id") ON DELETE CASCADE ON UPDATE CASCADE;

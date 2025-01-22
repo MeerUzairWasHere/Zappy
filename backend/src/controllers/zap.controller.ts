73f5d02ac9d97622d0a73709c9e5437f2c619992
Meer Uzair <meer.uxair007@gmail.com>
2025-01-22 22:26:24 +0530

    bug fixes

--- backend/src/controllers/zap.controller.ts
+++ backend/src/controllers/zap.controller.ts
@@ -1,18 +1,96 @@
 import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
-
 import { prismaClient } from "../db";
 import { ZapCreateInput } from "../types";
 import { NotFoundError } from "../errors";
+
+// export const createZap = async (
+//   req: Request<{}, {}, ZapCreateInput>,
+//   res: Response
+// ) => {
+//   const userId = req.user?.userId!;
+
+//   const { actions, zapName, availableTriggerId, triggerMetadata } = req.body;
+
+//   const triggerExists = await prismaClient.availableTrigger.findFirst({
+//     where: {
+//       id: availableTriggerId,
+//     },
+//   });
+
+//   if (!triggerExists) {
+//     throw new NotFoundError(
+//       `Trigger with ID '${availableTriggerId}' not found.`
+//     );
+//   }
+
+//   const actionExists = await prismaClient.availableAction.findMany({
+//     where: {
+//       id: {
+//         in: actions.map((x) => x.availableActionId), // Use the `in` operator
+//       },
+//     },
+//   });
+
+//   if (!actionExists || actionExists.length < actions.length) {
+//     const missingActions = actions
+//       .map((x) => x.availableActionId)
+//       .filter((id) => !actionExists.some((action) => action.id === id));
+//     console.log("Missing actions:", missingActions); // Debugging the missing actions  //TODO: We can not use same action twice, bug fix.
+//     throw new NotFoundError(
+//       `Actions not found for the following IDs: ${missingActions.join(", ")}`
+//     );
+//   }
+
+//   const zapId = await prismaClient.$transaction(async (tx) => {
+//     const zap = await prismaClient.zap.create({
+//       data: {
+//         zapName,
+//         userId,
+//         availableTriggerId: "",
+//         actions: {
+//           create: actions.map((x, index) => ({
+//             actionId: x.availableActionId,
+//             sortingOrder: index,
+//             metadata: x.actionMetadata,
+//           })),
+//         },
+//       },
+//     });
+
+//     const trigger = await tx.trigger.create({
+//       data: {
+//         triggerId: availableTriggerId,
+//         zapId: zap.id,
+//         metadata: triggerMetadata,
+//       },
+//     });
+
+//     await tx.zap.update({
+//       where: {
+//         id: zap.id,
+//       },
+//       data: {
+//         availableTriggerId: trigger.id,
+//       },
+//     });
+
+//     return zap.id;
+//   });
+
+//   res
+//     .status(StatusCodes.OK)
+//     .json({ msg: `Zap is created sucessfully with ID: ${zapId}` });
+// };
 
 export const createZap = async (
   req: Request<{}, {}, ZapCreateInput>,
   res: Response
 ) => {
   const userId = req.user?.userId!;
-
   const { actions, zapName, availableTriggerId, triggerMetadata } = req.body;
 
+  // Ensure the trigger exists
   const triggerExists = await prismaClient.availableTrigger.findFirst({
     where: {
       id: availableTriggerId,
@@ -25,26 +103,29 @@ export const createZap = async (
     );
   }
 
+  // Validate each action exists in the database
+  const actionIds = actions.map((x) => x.availableActionId);
   const actionExists = await prismaClient.availableAction.findMany({
     where: {
       id: {
-        in: actions.map((x) => x.availableActionId), // Use the `in` operator
+        in: actionIds,
       },
     },
   });
 
-  if (!actionExists || actionExists.length < actions.length) {
-    const missingActions = actions
-      .map((x) => x.availableActionId)
-      .filter((id) => !actionExists.some((action) => action.id === id));
-    console.log("Missing actions:", missingActions); // Debugging the missing actions  //TODO: We can not use same action twice, bug fix.
+  // Check for missing actions
+  const foundActionIds = actionExists.map((action) => action.id);
+  const missingActions = actionIds.filter((id) => !foundActionIds.includes(id));
+
+  if (missingActions.length > 0) {
     throw new NotFoundError(
       `Actions not found for the following IDs: ${missingActions.join(", ")}`
     );
   }
 
+  // Create the Zap and associated actions/trigger in a transaction
   const zapId = await prismaClient.$transaction(async (tx) => {
-    const zap = await prismaClient.zap.create({
+    const zap = await tx.zap.create({
       data: {
         zapName,
         userId,
@@ -81,7 +162,7 @@ export const createZap = async (
 
   res
     .status(StatusCodes.OK)
-    .json({ msg: `Zap is created sucessfully with ID: ${zapId}` });
+    .json({ msg: `Zap is created successfully with ID: ${zapId}` });
 };
 
 export const getAllZaps = async (req: Request, res: Response) => {

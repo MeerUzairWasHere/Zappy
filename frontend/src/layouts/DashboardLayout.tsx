59d53d21a3f1cbe288bd656c397c3ea2efb456fb
Meer Uzair <meer.uxair007@gmail.com>
2025-01-22 20:47:15 +0530

    Update DashboardLayout.tsx

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -1,10 +1,17 @@
 import customFetch from "@/utils/fetch";
 import { QueryClient, useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { Outlet, redirect, useNavigate, Navigate } from "react-router-dom";
+import {
+  Outlet,
+  redirect,
+  useNavigate,
+  Navigate,
+  useNavigation,
+} from "react-router-dom";
 import { useDashboardStore } from "@/store/dashboardStore";
 import { userQuery } from "@/lib/queries";
-import { MobileAppHeader, MobileSidebar, SideBar } from "@/components";
+import { Loading, MobileAppHeader, MobileSidebar, SideBar } from "@/components";
+import { ZapLayoutSkeleton } from "@/components/LoadingSkeletons";
 
 export const loader = (queryClient: QueryClient) => async () => {
   try {
@@ -21,6 +28,7 @@ export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
 
 const DashboardLayout = ({ queryClient }: { queryClient: QueryClient }) => {
   const navigate = useNavigate();
+  const navigation = useNavigation();
   useQuery(userQuery);
   const { isAuthError, setIsAuthError, logoutUser } = useDashboardStore();
 
@@ -54,7 +62,7 @@ const DashboardLayout = ({ queryClient }: { queryClient: QueryClient }) => {
         <MobileSidebar />
         <section className="flex-1 overflow-y-auto h-screen bg-[#E5E7EB] p-6">
           <MobileAppHeader />
-          <Outlet />
+          {navigation.state === "loading" ? <ZapLayoutSkeleton /> : <Outlet />}
         </section>
       </main>
     </ProtectedRoute>

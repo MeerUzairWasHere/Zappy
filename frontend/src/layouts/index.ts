33748073877dad7b3cde9291b428df1056288b9d
Meer Uzair <meer.uxair007@gmail.com>
2025-01-23 21:26:30 +0530

    Update index.ts

--- frontend/src/layouts/index.ts
+++ frontend/src/layouts/index.ts
@@ -1,3 +1,4 @@
 export { default as GlobalLayout } from "./GlobalLayout";
 export { default as DashboardLayout } from "./DashboardLayout";
 export { default as ZapsLayout } from "./ZapsLayout";
+export { default as SettingsLayout } from "./SettingsLayout";

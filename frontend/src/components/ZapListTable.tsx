cd1d9d77102b5d0a91798f748e99d4f42f73441b
Meer Uzair <meer.uxair007@gmail.com>
2025-01-28 23:30:13 +0530

    Update ZapListTable.tsx

--- frontend/src/components/ZapListTable.tsx
+++ frontend/src/components/ZapListTable.tsx
@@ -6,6 +6,7 @@ import DeleteZapModal from "./DeleteZapModal";
 import { zapsQuery } from "@/lib/queries";
 import { QueryClient, useQuery } from "@tanstack/react-query";
 import { redirect } from "react-router-dom";
+import NoZapFound from "./NoZapFound";
 
 export const loader = (queryClient: QueryClient) => async () => {
   try {
@@ -96,7 +97,7 @@ const ZapListTable = () => {
               </div>
             </>
           ) : (
-            "No zaps, create one!"
+            <NoZapFound />
           )}
         </>
       ) : (

c0ebb6cfce42cbbecba4f422ca75044ba81f9fc5
Meer Uzair <meer.uxair007@gmail.com>
2025-01-28 22:58:08 +0530

    Update ZapsPage.tsx

--- frontend/src/pages/ZapsPage.tsx
+++ frontend/src/pages/ZapsPage.tsx
@@ -1,19 +1,7 @@
 import { PageHeader, ZapListTable } from "@/components";
-import { zapsQuery } from "@/lib/queries";
-import { QueryClient, useQuery } from "@tanstack/react-query";
-import { redirect } from "react-router-dom";
-
-export const loader = (queryClient: QueryClient) => async () => {
-  try {
-    const { data } = await queryClient.ensureQueryData(zapsQuery);
-    return data;
-  } catch (error) {
-    return redirect("/");
-  }
-};
 
 const ZapsPage = () => {
-  const { data } = useQuery(zapsQuery);
+
   return (
     <>
       <PageHeader
@@ -22,7 +10,7 @@ const ZapsPage = () => {
         description="Create and customize your automation workflow"
         butttonText="Create"
       />
-      <ZapListTable zaps={data?.zaps} />
+      <ZapListTable />
     </>
   );
 };

e03c6ea92fffb5d6b996596737a3c219deffb28a
Meer Uzair <meer.uxair007@gmail.com>
2025-01-30 23:49:18 +0530

    Create dashboard.controller.ts

---
+++ backend/src/controllers/dashboard.controller.ts
@@ -0,0 +1,14 @@
+import { StatusCodes } from "http-status-codes";
+import { prismaClient } from "../db";
+import { Request, Response } from "express";
+
+export const getDashboardCount = async (req: Request, res: Response) => {
+  const activeZaps = await prismaClient.zap.count({
+    where: {
+      isActive: true,
+    },
+  });
+  const totalZaps = await prismaClient.zap.count();
+
+  res.status(StatusCodes.OK).json({ activeZaps, totalZaps });
+};

799b8be74b2b3ee1f0968248a71f412c43549cd4
Meer Uzair <meer.uxair007@gmail.com>
2025-02-01 20:52:00 +0530

    Update oauth.routes.ts

--- backend/src/routes/oauth.routes.ts
+++ backend/src/routes/oauth.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { Request, Response, NextFunction } from "express";
 import { authenticateUser } from "../middlewares/authentication";
-import { oAuthCallback, oAuthGmail } from "../controllers/oauth.controller";
+import { getConnections, oAuthCallback, oAuthGmail } from "../controllers/oauth.controller";
 
 const router = Router();
 
@@ -17,6 +17,13 @@ router.get(
   (req: Request, res: Response, next: NextFunction) =>
     authenticateUser(req, res, next),
   (req: Request, res: Response, next: NextFunction) => oAuthCallback(req, res)
+);
+
+router.get(
+  "/connections",
+  (req: Request, res: Response, next: NextFunction) =>
+    authenticateUser(req, res, next),
+  (req: Request, res: Response, next: NextFunction) => getConnections(req, res)
 );
 
 export default router;

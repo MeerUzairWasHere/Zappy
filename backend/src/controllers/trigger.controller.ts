d525d96239f45c785c5d8bf157c6907fa4e67af5
Meer Uzair <meer.uxair007@gmail.com>
2025-02-04 01:36:27 +0530

    Update trigger.controller.ts

--- backend/src/controllers/trigger.controller.ts
+++ backend/src/controllers/trigger.controller.ts
@@ -99,3 +99,99 @@ export const deleteAvailableTrigger = async (req: Request, res: Response) => {
     .status(StatusCodes.OK)
     .json({ msg: `Available Trigger with id: ${id} is deleted successfully!` });
 };
+
+// export const configureTrigger = async (req: Request, res: Response) => {
+//   const { id: zapId } = req.params;
+//   const { appId, config, triggerId } = req.body;
+
+//   const appExists = await prismaClient.app.findFirst({
+//     where: { id: appId },
+//   });
+
+//   if (!appExists) {
+//     throw new NotFoundError(`App with id: ${appId} does not exists!`);
+//   }
+
+//   const triggerExists = await prismaClient.availableTrigger.findFirst({
+//     where: { id: triggerId },
+//   });
+
+//   if (!triggerExists) {
+//     throw new NotFoundError(`Trigger with id: ${triggerId} does not exists!`);
+//   }
+
+//   const trigger = await prismaClient.trigger.create({
+//     data: {
+//       zapId,
+//       appId,
+//       triggerId,
+//       config,
+//     },
+//   });
+
+//   res.status(StatusCodes.OK).json(trigger);
+// };
+
+export const configureTrigger = async (req: Request, res: Response) => {
+  //TODO:  validate input
+  const userId = req.user?.userId;
+  const { zapId } = req.params;
+  const { triggerId, config, metadata } = req.body;
+
+  // Verify the Zap exists and belongs to the user
+  const existingZap = await prismaClient.zap.findUnique({
+    where: {
+      id: zapId,
+      userId: userId,
+    },
+  });
+
+  if (!existingZap) {
+    throw new NotFoundError("Zap not found.");
+  }
+
+  // Verify the trigger template exists
+  const availableTrigger = await prismaClient.availableTrigger.findUnique({
+    where: { id: triggerId },
+  });
+
+  if (!availableTrigger) {
+    throw new NotFoundError("Available Trigger not found.");
+  }
+
+  // Create or update trigger configuration
+  const trigger = await prismaClient.trigger.upsert({
+    where: {
+      zapId: zapId,
+    },
+    update: {
+      triggerId: availableTrigger.id,
+      metadata: config,
+    },
+
+    create: {
+      zapId,
+      config,
+      metadata,
+      triggerId: availableTrigger.id,
+      appId: availableTrigger.appId,
+    },
+    include: {
+      type: true,
+    },
+  });
+
+  // Update Zap status if needed
+  await prismaClient.zap.update({
+    where: { id: zapId },
+    data: {
+      status: "DRAFT",
+      availableTriggerId: trigger.id,
+    },
+  });
+
+  res.status(StatusCodes.OK).json({
+    message: "Trigger configured successfully",
+    trigger,
+  });
+};

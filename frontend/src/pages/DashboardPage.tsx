b6864f47a743f283db1c97602d05ceb8b84e3203
Meer Uzair <meer.uxair007@gmail.com>
2025-02-12 23:24:27 +0530

    Update DashboardPage.tsx

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -4,7 +4,6 @@ const DashboardPage = () => {
   return (
     <>
       <PageHeader
-        link={"zaps"}
         butttonText="Create Zap"
         title="Dashboard"
         description="Welcome to your dashboard"
